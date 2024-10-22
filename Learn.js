a01b7a9c4111cf88bd940648291a80c30dc0cd41
CodeBettr <greenlandselectric@gmail.com>
2024-10-22 05:59:02 +0000

    Testing_2

--- Learn.js
+++ Learn.js
@@ -17,3 +17,22 @@
     <button onclick="changeText()">Change Text</button>
 </body>
 </html>
+
+</head>!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change Text Example</title>
+    <script>
+        function changeText() {
+            document.getElementById("text").innerText = "Text has been changed!";
+        }
+    </script>
+</head>
+<body>
+    <h1>JavaScript Text Change Example</h1>
+    <p id="text">This is the original text.</p>
+    <button onclick="changeText()">Change Text</button>
+</body>
+</html>
